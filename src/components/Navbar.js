a9abd40319cf15080024e17640042af9ba19c2db
Tolkinxon <krosstony639@gmail.com>
2023-03-02 20:28:21 +0500

    replaced link

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import '../App.css'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+
+
+  return (
+    <nav>
+      <Link to="/">
+        Home
+      </Link>
+      <Link to="/about">
+        About
+      </Link>
+    </nav>
+  )
+}
+
+export default Navbar
