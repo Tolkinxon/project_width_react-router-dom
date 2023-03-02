31156efaab14e73cc0ada60693ad3d538ecf4551
Tolkinxon <krosstony639@gmail.com>
2023-03-02 21:02:14 +0500

    replaced link

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
+
+  const navigate = useNavigate()
+
   return (
-    <div>Home</div>
+    <>
+     <div>Home</div>
+     <button onClick={() => navigate('/about')}>go about page</button>
+    </>
+    
   )
 }
 
