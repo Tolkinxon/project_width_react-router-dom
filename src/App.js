0d9ce35b8ccf1a28c00383e7a8047c08f2c52211
Tolkinxon <krosstony639@gmail.com>
2023-03-03 23:08:33 +0500

    added concurrently package

--- src/App.js
+++ src/App.js
@@ -1,15 +1,5 @@
 import './App.css'
-import { Routes, Route } from 'react-router-dom'
-import Home from './components/Home'
-import About from './components/About'
-import Navbar from './components/Navbar'
-import NoMatched from './components/NoMatched'
-import Products from './components/Products'
-import Featured from './components/Featured'
-import New from './components/New'
-import User from './components/User'
-import UserElement from './components/UserElement'
-import Admin from './components/Admin'
+
 
 function App() {
   return (
