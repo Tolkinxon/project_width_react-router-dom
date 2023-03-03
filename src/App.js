67424967496fad02f175022cbf1b6e35728a4d24
Tolkinxon <krosstony639@gmail.com>
2023-03-03 23:28:30 +0500

    added Home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './components/Home'
 
 
 function App() {
   return (
     <>
-      
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
 
     </>
   )
---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import '../App.css'
+
+
+const Navbar = () => {
+  return (
+    <>
+        <nav>
+            <Link>Animals</Link>
+            {/* <Link>Animal</Link>
+            <Link>Animal</Link>
+            <Link>Animal</Link>
+            <Link>Animal</Link> */}
+        </nav>
+  
+    </>
+  )
+}
+
+export default Navbar
