786d1bc9c23b0464ca760cd1c031745e629c14b1
Tolkinxon <krosstony639@gmail.com>
2023-03-03 23:49:50 +0500

    added background

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
+import { useState } from 'react'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 
-
 function App() {
+  const [backgroundImage, setBackgroundImage] = useState(2)
+
+  setInterval(() => {
+    setBackgroundImage((prev) => prev + 1)
+    if (backgroundImage === 7) {
+      setBackgroundImage(0)
+    }
+  }, 1000)
+
   return (
     <>
-      <div className="App">
+      <div
+        className="App"
+        style={{ backgroundImage: `url(bg${backgroundImage}.jpg)` }}
+      >
         <Navbar />
         <Routes>
-          <Route path='/' element={<Home />} />
+          <Route path="/" element={<Home />} />
         </Routes>
       </div>
     </>
