d4e1880e77c8dfd45d3eff55ae79978912a6ae86
Tolkinxon <krosstony639@gmail.com>
2023-03-04 08:19:24 +0500

    icon added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,19 +2,18 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 import '../App.css'
 
-
 const Navbar = () => {
   return (
-    <> 
-        <nav>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to='animal'>Animals</NavLink>
-            <NavLink to='travel'>Travel</NavLink>
-            <NavLink to='nature'>Nature</NavLink>
-            <NavLink to='athletics'>Athletics</NavLink>
-            <NavLink to='food'>Food</NavLink>
-        </nav>
-  
+    <>
+      <nav>
+        <i class="fa-solid fa-camera"></i>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="animal">Animals</NavLink>
+        <NavLink to="travel">Travel</NavLink>
+        <NavLink to="nature">Nature</NavLink>
+        <NavLink to="athletics">Athletics</NavLink>
+        <NavLink to="food">Food</NavLink>
+      </nav>
     </>
   )
 }
