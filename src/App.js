45378d60e1133300f66ebfab87564950fbb914a2
Tolkinxon <krosstony639@gmail.com>
2023-03-04 07:14:38 +0500

    added syles of a tag

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Routes, Route } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
+import Animal from './components/Animal'
+import Travel from './components/Travel'
 
 function App() {
   const [backgroundImage, setBackgroundImage] = useState(1)
@@ -30,6 +32,8 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path='animal' element={<Animal />} />
+          <Route path='travel' elemant={<Travel />} />
         </Routes>
       </div>
     </>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,17 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import '../App.css'
 
 
 const Navbar = () => {
   return (
-    <>
+    <> 
         <nav>
-            <Link>Animals</Link>
-            {/* <Link>Animal</Link>
-            <Link>Animal</Link>
-            <Link>Animal</Link>
-            <Link>Animal</Link> */}
+            <NavLink to='animal'>Animals</NavLink>
+            <NavLink to='travel'>Travel</NavLink>
+            {/* <NavLink>Nature</NavLink>
+            <NavLink>Athletics</NavLink>
+            <NavLink>Food & Drink</NavLink> */}
         </nav>
   
     </>
