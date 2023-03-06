1c8e7f3012ce4714c4a40475ee6cf08f0c9b202c
Tolkinxon <krosstony639@gmail.com>
2023-03-06 23:52:44 +0500

    getting inform

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { getDataTravel } from '../services/getData'
+import '../App.css'
 
 const Categories = () => {
   const [dataTravel, setDataTravel] = useState([])
@@ -16,7 +17,48 @@ const Categories = () => {
 
   console.log(dataTravel)
 
-  return <div>Categories</div>
+  return (<>
+    
+    <div className="photo-container">
+          <span>
+            {dataTravel.map((item, idx) => (
+              <>
+                { idx % 3 === 0 &&  <img
+                  key={idx}
+                  src={item.node.photoUrl[0].url}
+                  alt="animal-photo"
+                />}
+              
+              </>
+            ))}
+          </span>
+          <span>
+            {dataTravel.map((item, idx) => (
+              <>
+                { (idx + 1) % 3 === 0 &&  <img
+                  key={idx}
+                  src={item.node.photoUrl[0].url}
+                  alt="animal-photo"
+                />}
+              
+              </>
+            ))}
+          </span>
+          <span>
+            {dataTravel.map((item, idx) => (
+              <>
+                { (idx + 2) % 3 === 0 &&  <img
+                  key={idx}
+                  src={item.node.photoUrl[0].url}
+                  alt="animal-photo"
+                />}
+              
+              </>
+            ))}
+          </span>
+     
+        </div>
+  </>)
 }
 
 export default Categories
