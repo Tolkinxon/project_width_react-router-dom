7055a4ae1eac9e462c01e066e22c4ab178301188
Tolkinxon <krosstony639@gmail.com>
2023-03-08 23:31:31 +0500

    deleted some components

--- src/components/Athletics.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Athletics = () => {
-  return (
-    <div>Athletics</div>
-  )
-}
-
-export default Athletics
--- src/components/Nature.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Nature = () => {
-  return (
-    <div className='nature'>Nature</div>
-  )
-}
-
-export default Nature
